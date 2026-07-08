477994ad1a00726f0242e156e032949a227e060f
James Berry <james@coppermoth.com>
2026-07-08 10:05:11 +0100

    Add /healthz route and -healthcheck self-probe

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"text/template"
 	"time"
 
@@ -15,7 +17,16 @@ var pokestopTemplate *template.Template
 var gymTemplate *template.Template
 
 func main() {
+	healthcheck := flag.Bool("healthcheck", false, "probe /healthz on the local server and exit 0 (healthy) or 1")
+	flag.Parse()
+
 	cfg, err := Load()
+	if *healthcheck {
+		if err != nil {
+			os.Exit(1)
+		}
+		os.Exit(runHealthcheck(cfg.Port))
+	}
 	if err != nil {
 		log.Fatalf("config: %v", err)
 	}
@@ -53,6 +64,7 @@ func main() {
 	r.GET("/pokemon/:pokemon_id/:template", GetPokemon)
 	r.GET("/pokestop/:pokestop_id/:template", GetPokestop)
 	r.GET("/gym/:gym_id/:template", GetGym)
+	r.GET("/healthz", func(c *gin.Context) { c.String(http.StatusOK, "ok") })
 
 	srv := &http.Server{
 		Addr:    fmt.Sprintf(":%d", config.Port),
@@ -60,4 +72,17 @@ func main() {
 	}
 	fmt.Printf("%s [] Starting server on port %d\n", time.Now().Format(config.TimestampFormat), config.Port)
 	srv.ListenAndServe()
+}
+
+func runHealthcheck(port int) int {
+	client := &http.Client{Timeout: 2 * time.Second}
+	resp, err := client.Get(fmt.Sprintf("http://127.0.0.1:%d/healthz", port))
+	if err != nil {
+		return 1
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return 1
+	}
+	return 0
 }
