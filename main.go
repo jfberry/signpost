99d420d027fde951a62eda1676bf81d4767ed9e4
James Berry <james@coppermoth.com>
2026-07-08 10:01:03 +0100

    Load config via Load() and fail fast with a clear message

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"log"
 	"net/http"
-	"os"
 	"text/template"
-    "time"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pelletier/go-toml/v2"
 )
 
 var pokemonTemplate *template.Template
@@ -17,21 +15,11 @@ var pokestopTemplate *template.Template
 var gymTemplate *template.Template
 
 func main() {
-	tomlFile, err := os.Open("config.toml")
-	// if we os.Open returns an error then handle it
+	cfg, err := Load()
 	if err != nil {
-		panic(err)
+		log.Fatalf("config: %v", err)
 	}
-	// defer the closing of our tomlFile so that we can parse it later on
-	defer tomlFile.Close()
-
-	byteValue, _ := io.ReadAll(tomlFile)
-
-	err = toml.Unmarshal(byteValue, &config)
-	if err != nil {
-		panic(err)
-	}
-	config.TimestampFormat = "2006-01-02 15:04:05"
+	config = cfg
 
 	//	templateStr := "https://maps.google.com/maps?q={{.lat}},{{.lon}}"
 
